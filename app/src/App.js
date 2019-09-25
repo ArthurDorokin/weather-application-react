0f6cadb7fddb6c24d86d8ec8141df2a8b77acd20
Артур <a.u.dorokhin@gmail.com>
2019-09-25 23:47:42 +0300

    new components

---
+++ app/src/App.js
@@ -0,0 +1,61 @@
+import React from "react";
+import Info from "./components/info";
+import Form from "./components/form";
+import Weather from "./components/weather";
+
+const API_KEY = "dddf9f35ba14b846f68ac0564bb924d8";
+
+class App extends React.Component {
+
+    state = {
+        temp: undefined,
+        city: undefined,
+        country: undefined,
+        pressure: undefined,
+        sunset: undefined,
+        error: undefined
+    };
+
+    gettingWeather = async (e) => {
+        e.preventDefault();
+        const city = e.target.elements.city.value;
+
+        if(city){
+            const api_url = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+            const data = await api_url.json();
+
+            let sunset = data.sys.sunset;
+            let date = new Date();
+            date.setTime(sunset);
+            let sunset_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+            this.setState({
+                temp: data.main.temp,
+                city: data.name,
+                country: data.sys.country,
+                pressure: data.main.pressure,
+                sunset: sunset_date,
+                error: ""
+            });
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                <Info/>
+                <Form weatherMethod={this.gettingWeather}/>
+                <Weather
+                temp={this.state.temp}
+                city={this.state.city}
+                country={this.state.country}
+                pressure={this.state.pressure}
+                sunset={this.state.sunset}
+                error={this.state.error}
+                />
+            </div>
+        );
+    }
+}
+
+export default App;
